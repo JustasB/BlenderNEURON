22ec65709564ae66ec2da8fc91e54fe69d13710e
Justas Birgiolas <jbirgio@gmail.com>
2018-08-19 10:03:18 -0700

    Static files fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,16 +45,17 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['files_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = '.md'
 
 # The master toctree document.
 master_doc = 'index'
@@ -91,7 +92,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['files_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
