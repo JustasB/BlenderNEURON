abf8cda5ec311797b6b5603b907de1c7c0df9354
Justas Birgiolas <jbirgio@gmail.com>
2020-05-15 18:23:56 -0400

    group filter fix

--- blenderneuron/blender/blenderrootgroup.py
+++ blenderneuron/blender/blenderrootgroup.py
@@ -209,7 +209,7 @@ class BlenderRootGroup(RootGroup):
 
         else:
             for root in self.node.root_index.values():
-                if not fnmatch(root.name.lower(), pattern):
+                if not fnmatch(root.name, pattern):
                     continue
 
                 if condition == 'All':
