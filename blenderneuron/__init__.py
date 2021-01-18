08957f06fde3b79ce88df1bc4dda06a6a185c688
Robin De Schepper <robin.deschepper93@gmail.com>
2021-01-18 18:11:24 +0100

    Added `inside_blender` check

--- blenderneuron/__init__.py
+++ blenderneuron/__init__.py
@@ -1,25 +1,33 @@
-# `bl_info` is read without evaluating the module. The BlenderNEURON installer will
-# replace the `blender` and `location` key with what is appropriate for the detected
-# Blender version so that our addon is compatible across Blender versions.
-bl_info = {
-    "name": "BlenderNEURON",
-    "description": "A Blender GUI for NEURON simulator",
-    "author": "Justas Birgiolas",
-    "version": (3, 0),
-    "blender": (2, 91, 0),
-    "location": "View3D > Properties > BlenderNEURON",
-    "wiki_url": "BlenderNEURON.org",
-    "tracker_url": "https://github.com/JustasB/BlenderNEURON/issues",
-    "support": "COMMUNITY",
-    "category": "Import-Export",
-}
+try:
+    import bpy
+except:
+    inside_blender = False
+else:
+    inside_blender = True
 
-from ._blender import make_blender_addon
+if inside_blender:
+    # `bl_info` is read without evaluating the module. The BlenderNEURON installer will
+    # replace the `blender` and `location` key with what is appropriate for the detected
+    # Blender version so that our addon is compatible across Blender versions.
+    bl_info = {
+        "name": "BlenderNEURON",
+        "description": "A Blender GUI for NEURON simulator",
+        "author": "Justas Birgiolas",
+        "version": (3, 0),
+        "blender": (2, 91, 0),
+        "location": "View3D > Properties > BlenderNEURON",
+        "wiki_url": "BlenderNEURON.org",
+        "tracker_url": "https://github.com/JustasB/BlenderNEURON/issues",
+        "support": "COMMUNITY",
+        "category": "Import-Export",
+    }
 
-addon = make_blender_addon()
+    from ._blender import make_blender_addon
 
-def register():
-    addon.register()
+    addon = make_blender_addon()
 
-def unregister():
-    addon.unregister()
+    def register():
+        addon.register()
+
+    def unregister():
+        addon.unregister()
