0047992ca505b852c5be0387a2194605fb77d53a
Robin De Schepper <robin.deschepper93@gmail.com>
2021-01-19 10:23:54 +0100

    Created the base public API for #21

--- blenderneuron/__init__.py
+++ blenderneuron/__init__.py
@@ -1,3 +1,6 @@
+import warnings
+import numpy as np
+
 try:
     import bpy
 except:
@@ -24,10 +27,131 @@ if inside_blender:
 
     from ._blender import make_blender_addon
 
-    addon = make_blender_addon()
+    addon, controller = make_blender_addon()
 
     def register():
         addon.register()
 
     def unregister():
         addon.unregister()
+
+else:
+    class Reporter:
+        @property
+        def is_available(self):
+            return False
+
+        def __getattr__(self, name):
+            warnings.warn("Controller can't be used outside of Blender environment", stacklevel=2)
+            return Reporter()
+
+        def __call__(self, *args, **kwargs):
+            warnings.warn("Controller can't be used outside of Blender environment", stacklevel=2)
+
+    controller = Reporter()
+
+class Branch:
+    """
+    A branch is a piece of uninterrupted unbranching cable used to construct
+    :class:`cells <.Cell>`.
+    """
+    def __init__(self, coords, children=None, ref=None):
+        """
+        Create a branch.
+
+        :param coords: A matrix or list of 3d points describing the branch in space.
+        :type coords: np.ndarray (or list of lists-like outside Blender)
+        :param children: A collection of branches that are the continuation of this branch
+        :type children: list
+        :param ref: A reference hash to associate to this object. Can be used to map this
+            object to its data source.
+        """
+        if children is None:
+            children = []
+        self._children = children
+        self._coords = coords
+        self._cell = None
+        self._ref = ref
+
+    @property
+    def coords(self):
+        """
+        A list of 3d coordinates that describe this piece of cell morphology
+        """
+        if self._cell:
+            raise NotImplementedError("Reading Blender state not supported yet.")
+        else:
+            return self._coords
+
+    @coords.setter
+    def set_coords(self):
+        if self._cell:
+            raise NotImplementedError("Manipulating the Blender object state not supported yet.")
+        else:
+            self._coords = coords
+
+    def to_dict(self):
+        d = dict(coords=self.coords)
+        if self._ref is not None:
+            d["ref"] = self.ref
+        return d
+
+
+class Cell:
+    """
+    A cell is the 3D representation of a collection of root :class:`Branches <.Branch>`,
+    branching out into child Branches.
+    """
+    def __init__(self, roots):
+        self._roots = roots
+
+    def register(self):
+        """
+        Register the cell with the controller to create its Blender object and manage its
+        state. Only available inside Blender.
+        """
+        controller.register_cell(self)
+
+    @property
+    def roots(self):
+        """
+        A list of 3d coordinates that describe this piece of cell morphology
+        """
+        if self._cell:
+            raise NotImplementedError("Reading Blender state not supported yet.")
+        else:
+            return self._roots
+
+    @roots.setter
+    def set_roots(self):
+        if self._cell:
+            raise NotImplementedError("Manipulating the Blender object state not supported yet.")
+        else:
+            self._roots = roots
+
+    def __dissolve__(self):
+        for root in roots:
+            root.__dissolve__()
+
+def create_branch(coords):
+    """
+    Create a new :class:`.Branch`
+    """
+    return Branch(coords)
+
+
+def create_cell(roots, register=inside_blender):
+    """
+    Create a new :class:`.Cell` from the given roots.
+
+    :param roots: Collection of :class:`Branches <.Branch>` without parents that start a
+        branch of the cell morphology.
+    :type roots: iterable
+    :param register: Register the cell with the controller to create its Blender object
+        and manage its state. Only available inside Blender.
+    :type register: bool
+    """
+    cell = Cell(roots)
+    if register:
+        cell.register()
+    return cell
