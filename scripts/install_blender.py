159c7909db29782dc9ad6c736357ca34ca6bbb1c
Justas Birgiolas <JustasB@users.noreply.github.com>
2024-08-04 11:24:36 -0700

    update blender download script

--- scripts/install_blender.py
+++ scripts/install_blender.py
@@ -3,16 +3,16 @@ import os, re
 skip_download = os.path.exists('./blender')
 
 if not skip_download:
-    latest_url = 'https://mirror.clarkson.edu/blender/release/Blender2.79/blender-2.79b-linux-glibc219-x86_64.tar.bz2'
+    latest_url = 'https://mirror.clarkson.edu/blender/release/Blender3.5/blender-3.5.1-linux-x64.tar.xz'
 
     print('Downloading Blender', latest_url)
     import os
-    os.system('curl -L -o blender.tar.bz2 ' + latest_url)
-    os.system('tar xjf blender.tar.bz2')
+    os.system('curl -L -o blender.tar.xz ' + latest_url)
+    os.system('tar xJf blender.tar.xz')
 
     # rename dir to just blender
     d = '.'
-    new_dir = os.path.join(d,'blender')
+    new_dir = os.path.join(d, 'blender')
     print('Renaming dir to', new_dir)
 
     blender_dir = next(os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o)) and o.startswith('blender-'))
