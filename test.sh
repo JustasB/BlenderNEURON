373ea116e9403cb4656cc1234bf0d55540f96e9c
Justas Birgiolas <JustasB@users.noreply.github.com>
2024-08-04 12:19:21 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 rm -rf htmlcov
 coverage erase
 
-python -m unittest discover -v --start-directory tests
+coverage run -m unittest discover -v --start-directory tests
 
 coverage combine
 coverage html
