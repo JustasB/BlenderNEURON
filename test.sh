2f79440f6ab2750b30f73e60c3f89aa5a9c08a56
Justas Birgiolas <JustasB@users.noreply.github.com>
2024-08-11 08:10:11 -0700

    update dockerfile and coverage fix

--- test.sh
+++ test.sh
@@ -2,7 +2,12 @@ rm -rf htmlcov
 coverage erase
 
 export COVERAGE_PROCESS_START=.coveragerc
+
+# Run all tests under [repo]/tests
 coverage run -m unittest discover -v --start-directory tests
+
+# Single test example:
+#coverage run -m unittest tests.test_blender_node.TestBlenderNode.test_add_remove_group_and_cells
 
 coverage combine
 coverage html
