dc44c91e03d1b6f589e94a39564cfd49fa50b34b
Justas Birgiolas <JustasB@users.noreply.github.com>
2024-09-01 17:02:59 -0700

    coverage test (#38)
    * coverage test
    
    * coverage fix
    
    * coverage fix
    
    * Create codecov.yml
    
    * Delete codecov.yml

--- test.sh
+++ test.sh
@@ -10,5 +10,6 @@ coverage run -m unittest discover -v --start-directory tests
 #coverage run -m unittest tests.test_blender_node.TestBlenderNode.test_add_remove_group_and_cells
 
 coverage combine
+coverage xml
 coverage html
 coverage report
