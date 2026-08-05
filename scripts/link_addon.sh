c3307456a402a4185b9fecc0280aeb73720aa142
Justas Birgiolas <10732358+JustasB@users.noreply.github.com>
2026-08-04 20:09:05 -0400

    ci fix

--- scripts/link_addon.sh
+++ scripts/link_addon.sh
@@ -2,8 +2,9 @@
 echo 'cd /BlenderNEURON' >> /root/.bashrc
 
 # Create a symbolic link from folder mapped to container to Blender addon folder
-mkdir -p /blender/5.2/scripts/addons
-ln -sfn /BlenderNEURON/blenderneuron /blender/5.2/scripts/addons/blenderneuron
+export BLENDER_SYSTEM_SCRIPTS=/BlenderNEURON/blender_scripts
+mkdir -p $BLENDER_SYSTEM_SCRIPTS/addons
+ln -sfn /BlenderNEURON/blenderneuron $BLENDER_SYSTEM_SCRIPTS/addons/blenderneuron
 
 # Enable the addon within blender ('check the box')
 blender/blender --python enable_addon.py --background -noaudio
